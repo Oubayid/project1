c58543ba0f390e167be4765639ae738d0592f4aa
oubayid <oubaidbensaid18910@gmail.com>
2023-05-09 01:45:30 +0200

    70

---
+++ main.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+    $(".cpu").click(function(){
+      $(".item").slideUp("fast");
+    });
+  });
+
+
+  $(document).ready(function(){
+    $(".gpu").click(function(){
+      $(".itemofgpu").slideUp("slow");
+    });
+  });
+
+
+  $(document).ready(function(){
+    $(".cpu").click(function(){
+      $(".item").toggle();
+    });
+  });
+
+
+  $(document).ready(function(){
+    $(".gpu").click(function(){
+      $(".itemofgpu").toggle();
+    });
+  });
+
+  $(document).ready(function(){
+    $(".Motherboard").click(function(){
+      $(".item1").slideUp("slow");
+    });
+  });
+
+  
+  $(document).ready(function(){
+    $(".Motherboard").click(function(){
+      $(".item1").toggle();
+    });
+  });
+
+
+
+  
